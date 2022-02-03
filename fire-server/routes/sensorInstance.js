f8e54115118d9b16a134b9dee6c290914c1dc9ac
RUBY PILLAR <pillar.ruby@gmail.com>
2022-02-03 14:57:39 +0100

    +calculate FDI on post

--- fire-server/routes/sensorInstance.js
+++ fire-server/routes/sensorInstance.js
@@ -15,6 +15,7 @@ router.post('/'), (req, res) => {
 
   let newSensorInstance = new SensorInstance(req.body.sensorInstance)
   // TODO calculate the fdi
+  newSensorInstance.fdi=2 * Math.pow(10, -0.45 + 0.987 * Math.log(5)-0.0345 * newSensorInstance.humidity + 0.0338 * newSensorInstance.temperature+ 0.0234 * newSensorInstance.windSpeed);
   newSensorInstance.save((err, insertedSensorInstance) => {
     if(err) res.statusCode(300);
     res.send(insertedSensorInstance);
