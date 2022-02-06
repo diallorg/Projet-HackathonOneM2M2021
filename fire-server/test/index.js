20b4ff8948926f77891e23f701df102ead752f14
Alexandre Viale <tutunelel@gmail.com>
2022-02-06 21:50:04 +0100

    +add mocha for tests

---
+++ fire-server/test/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const config = require('../middleware/config');
+const mongoose = require('mongoose');
+const SensorInstance = require('../model/SensorInstanceModel');
+
+describe('Fire Server tests launched', function() {
+  var app,
+      date;
+
+  this.timeout(10000);
+
+  before(function(done) {
+    app = express();
+    app.listen(config.AEPort, function(err) {
+      if (err) { return done(err); }
+      done();
+    });
+  });
+
+  beforeEach(function() {
+    date = new Date();
+  });
+
+  after(function() {
+    console.log("Tests passed successfully");
+  });
+
+  afterEach(function() {
+    console.log("At", date);
+  });
+
+  it('should connect to mongodb database', function(done) {
+    mongoose.connect(config.mongoDBLink).then(() => {
+      done();
+    }, err => {
+      throw new Error(err.message);
+    })
+  });
+
+  it('should get data from database', function(done) {
+    SensorInstance.findOne({}).then(() => {
+      done();
+    }, err => {
+      throw new Error(err.message);
+    })
+  });
+
+});
