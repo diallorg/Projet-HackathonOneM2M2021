180a00cb73cf3b2a698327ce7bade80542571136
Pacito2 <you@example.com>
2022-02-24 13:35:01 +0100

    Adding mocha test to fire-map

--- fire-map/src/pages/statistics/components/chart.vue
+++ fire-map/src/pages/statistics/components/chart.vue
@@ -6,7 +6,7 @@ export default {
   props: ['data', 'options'],
   mounted () {
     // Overwriting base render method with actual data.
-    this.renderChart(this.data, this.options)
+    this.renderChart(this.data, this.options);
   }
 }
 </script>
