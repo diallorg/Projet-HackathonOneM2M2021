88247a51f536d8cd82a0ef87798d368334a10ee6
Alexandre Viale <tutunelel@gmail.com>
2022-03-03 15:08:52 +0100

    -useless files

--- fire-server/model/MiddleNodeModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MiddleNodeSchema = new mongoose.Schema({
-  name: String,
-  poa: String,
-});
-const MiddleNode = mongoose.model('middlenode', MiddleNodeSchema);
-
-module.exports = MiddleNode;
