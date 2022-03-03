b37931eb29fa8dc8424309d54bb70cc6891733a0
Alexandre Viale <tutunelel@gmail.com>
2022-03-03 09:51:22 +0100

    fix requires

--- fire-server/index.js
+++ fire-server/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const cors =  require('cors');
 const mongoose = require('mongoose');
-// const discover = require('./middleware/discover');
-// const subscribe = require('./middleware/subscribe');
 const config = require('./middleware/config')
 
 // Body Parser Middleware and Cors
@@ -12,15 +10,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // Set api routes
-app.use('/fires', require('./routes/fires'));
-app.use('/hydrants', require('./routes/hydrants'));
-app.use('/prevents', require('./routes/prevents'));
 app.use('/sensor-instances', require('./routes/sensorInstance'));
 // IN receivers
-app.use('/send/humidity', require('./routes/inReceivers/humidity'));
-app.use('/send/flame', require('./routes/inReceivers/flame'));
-app.use('/send/temperature', require('./routes/inReceivers/temperature'));
-app.use('/send/windSpeed', require('./routes/inReceivers/windSpeed'));
 app.get("/", (req,res) => {
   console.log("Received get request on root.");
   console.log(req);
